e7427aaecf1421daf7cf8c1efca23b6bf5d2c9ba
AnnaShiraishi <57167266+AnnaShiraishi@users.noreply.github.com>
2022-03-20 16:00:18 +0800

    delete
    1

--- src/main/java/org/csu/jpetstore/service/impl/AccountServiceImpl.java
+++ src/main/java/org/csu/jpetstore/service/impl/AccountServiceImpl.java
@@ -15,24 +15,6 @@ public class AccountServiceImpl implements AccountService {
     }
 
     @Override
-    public Account getAccount(String username, String password) {
-        accountDAO = new AccountDAOImpl();
-        Account account = new Account();
-        account.setUsername(username);
-        account.setPassword(password);
-        return accountDAO.getAccountByUsernameAndPassword(account);
-    }
-    
-
-    @Override
-    public void insertAccount(Account account) {
-        accountDAO = new AccountDAOImpl();
-        accountDAO.insertAccount(account);
-        accountDAO.insertProfile(account);
-        accountDAO.insertSignon(account);
-    }
-
-    @Override
     public void updateAccount(Account account) {
         accountDAO = new AccountDAOImpl();
         accountDAO.updateAccount(account);
